23bf6e0afbc6bf68d2b454e759167ead68798a03
tushal kumar <tushalkashyap11@gmail.com>
2021-07-08 23:37:30 -0700

    Added: code to use set event listiner for name and salary

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -57,11 +57,3 @@ class EmployeePayrollData {
 
 
 
-// //uc8 set the event lisitner in salary range
-
-// const salary = document.querySelector('#salary');
-// const output = document.querySelector('.salary-output');
-// output.textContent = salary.value;
-// salary.addEventListener('input', function() {
-//     output.textContent = salary.value;
-// });
