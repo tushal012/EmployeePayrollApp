846799a49be2873f5e7cf6df683aca0891cb59ae
tushal kumar <tushalkashyap11@gmail.com>
2021-07-08 20:12:27 -0700

    Added: event lisitner in salary range to display value

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,3 +1,5 @@
+//uc8 set the event lisitner in salary range
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
