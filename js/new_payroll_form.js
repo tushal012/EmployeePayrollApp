a0b3c01fecbb2535e9cef6d3047cb6ea711edd64
tushal kumar <tushalkashyap11@gmail.com>
2021-07-11 00:06:19 -0700

    payroll object code resolved

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -1,81 +1,71 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const name = document.querySelector('#name');
-    const textError = document.querySelector('.text-error');
-    name.addEventListener('input', function() {
+    const name = document.querySelector("#name");
+    const nameError = document.querySelector(".name-error");
+    name.addEventListener("input", function () {
         if (name.value.length == 0) {
-            textError.textContent = "";
+            nameError.textContent = "";
             return;
         }
         try {
-            let employeePayrollData = new EmployeePayrollData();
-            employeePayrollData.name = name.value;;
-            textError.textContent = "";
-        } catch (e) {
-            textError.textContent = e;
+            (new EmployeePayrollData()).name = name.value;
+            nameError.textContent = "";
+        } catch (error) {
+            nameError.textContent = error;
         }
     });
+
 
     const salary = document.querySelector('#salary');
     const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-    salary.addEventListener('input', function() {
-        output.textContent = salary.value
+    salary.addEventListener('input', function () {
+        output.textContent = salary.value;
     });
 });
 
+
 const save = () => {
     try {
-        let employeePayrollData = createEmployeepayrolldata();
-    } catch (e) {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (error) {
         return;
     }
 }
 
-const createEmployeepayrolldata = () => {
+const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
     try {
-        employeePayrollData.name = getInputValueById('#name');
-    } catch (e) {
-        setTextValue('.text-error', e);
-        throw e;
+        employeePayrollData.name = getInputValueById("#name");
+    } catch (error) {
+        setTextValue('.name-error', error);
+        throw error;
     }
-    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
-    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
-    employeePayrollData.department = getSelectedValues('[name=department]');
-    employeePayrollData.salary = getInputValueById('#salary');
-    employeePayrollData.note = getInputValueById('#notes');
-    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " +
-        getInputValueById('#year');
-    employeePayrollData.date = Date.parse(date);
+    employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
+    employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
+    employeePayrollData.department = getSelectedValues("[name = department]");
+    employeePayrollData.salary = getInputValueById("#salary");
+    employeePayrollData.notes = getInputValueById("#notes");
+    employeePayrollData.startDate = new Date(getInputValueById("#year"), getInputValueById("#month"), getInputValueById("#day"));
     alert(employeePayrollData.toString());
     return employeePayrollData;
 }
 
-const getSelectedValues = (propertyValue) => {
-    let allItems = document.querySelectorAll(propertyValue);
-    let selItem = [];
-    allItems.forEach(item => {
-        if (item.checked) selItem.push(item.value);
-    });
-    return selItem;
+const setTextValue = (id,value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
 }
 
-/**
- * 1: querySelector is the newer feature.
- * 2: The querySelectore method can be used when selectin by element name,
- *    nesting, or class name.
- * 3: querySelector lets you find elements with rules that can,t be expressed with getElementbyId
- */
+const getSelectedValues = (property) => {
+    let allItems = document.querySelectorAll(property);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            selectedItems.push(item.value);
+    });
+    return selectedItems;
+}
+
 const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
-    return value;
-}
-
-/**
- * 1: getElementById is better supported than  querySelector in older versions of the browsers.
- * 2: The thing with getElementById is that it only allows to select an element by its id.
- */
-const getInputElementValue = (id) => {
-    let value = document.getElementById(id).value;
     return value;
 }
