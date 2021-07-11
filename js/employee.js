925ccbed973e89a0fa5263c3c50546c641e0213e
tushal kumar <tushalkashyap11@gmail.com>
2021-07-10 21:18:48 -0700

    Added: functionlity to validate name and start date

--- js/employee.js
+++ js/employee.js
@@ -12,8 +12,13 @@ class EmployeePayRollData {
 
     get name() { return this._name; }
     set name(name) {
-        this._name = name;
+        let regexName = RegExp('^[A-Z][a-z]{3,}$');
+        if (regexName.test(name))
+            this._name = name;
+        else
+            throw 'Name is Invalid!';
     }
+
     get profilePic() { return this._profilePic; }
     set profilePic(profilePic) { this._profilePic = profilePic; }
 
@@ -21,16 +26,21 @@ class EmployeePayRollData {
     set salary(salary) {
         this._salary = salary;
     }
+
     get gender() { return this._gender; }
     set gender(gender) {
         this._gender = gender;
     }
+
     get department() { return this._department; }
     set department(department) { this._department = department; }
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
-        this._startDate = startDate;
+         if (startDate <= new Date())
+            this._startDate = startDate;
+        else
+            throw 'StartDate is Invalid!';
     }
 
     get notes() { return this._notes; }
@@ -48,16 +58,24 @@ class EmployeePayRollData {
 }
 
 function save() {
+
     let employeePayrollData = new EmployeePayRollData();
-    employeePayrollData.name = getInputValueById("#name");
-    employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
-    employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
-    employeePayrollData.department = getSelectedValues("[name = department]");
-    employeePayrollData.salary = getInputValueById("#salary");
-    employeePayrollData.notes = getInputValueById("#notes");
-   employeePayrollData.startDate = new Date(getInputValueById("#year"), getInputValueById("#month"), getInputValueById("#day"));
-   alert(employeePayrollData.toString());
+
+    try {
+        employeePayrollData.name = getInputValueById("#name");
+        employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
+        employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
+        employeePayrollData.department = getSelectedValues("[name = department]");
+        employeePayrollData.salary = getInputValueById("#salary");
+        employeePayrollData.notes = getInputValueById("#notes");
+        employeePayrollData.startDate = new Date(getInputValueById("#year"), getInputValueById("#month"), getInputValueById("#day"));
+        alert(employeePayrollData.toString());
+    }
+    catch (error) {
+        console.log(error);
+    }
 }
+
 
 let getSelectedValues = (property) => {
     let allItems = document.querySelectorAll(property);
